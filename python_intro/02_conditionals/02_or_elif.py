84fd0cf61a6c27a85276da392da68b750984afd6
Endre Synnes <endre.m.synnes@gmail.com>
2021-01-24 17:06:54 +0100

    simplified 02_or_elif

--- python_intro/02_conditionals/02_or_elif.py
+++ python_intro/02_conditionals/02_or_elif.py
@@ -1,5 +1,5 @@
 
-job_title = input()
+job_title = "intern"
 
 # OR
 if job_title == "cto" or job_title == "director":
@@ -9,6 +9,8 @@ else:
 
 # Elif
 if job_title == "cto":
-    print("Welcome CTO")
+    print("Welcome CTO, you are awesome")
 elif job_title == "director":
-    print("Welcome Director")
+    print("Welcome Director, you are very kind")
+else:
+    print(job_title + " are not welcome here.")
