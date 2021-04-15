a9fa7a791c4a9c31fb0190b0be537eb427f4c3ab
Endre Synnes <endre.m.synnes@gmail.com>
2021-04-15 19:40:31 +0200

    updated nested loop example

---
+++ python_intro/03_loops/09_nested_loops.py
@@ -0,0 +1,48 @@
+# Ex 1 Using a nested loop to display multiplication tables from 1-10.
+
+# 1 2 3 4 5 6 7 8 9 10 
+# 2 4 6 8 10 12 14 16 18 20 
+# 3 6 9 12 15 18 21 24 27 30 
+# 4 8 12 16 20 24 28 32 36 40 
+# 5 10 15 20 25 30 35 40 45 50 
+# 6 12 18 24 30 36 42 48 54 60 
+# 7 14 21 28 35 42 49 56 63 70 
+# 8 16 24 32 40 48 56 64 72 80 
+# 9 18 27 36 45 54 63 72 81 90 
+# 10 20 30 40 50 60 70 80 90 100 
+
+
+for i in range(1,11):
+   for j in range(1,11):
+      print (j*i, end=' ')
+   print()
+
+
+# Ex 2 More complex objects with a nested list.
+birds = [
+  {
+    "name": "eagle",
+    "colors": [
+      "brown",
+      "white",
+      "yellow"
+    ]
+  },
+  {
+    "name": "parrot",
+    "colors": [
+      "red",
+      "yellow",
+      "green",
+      "blue",
+      "white"
+    ]
+  }
+]
+
+for bird in birds:
+  print(f"{bird['name']} colors:")
+
+  for color in bird['colors']:
+    print(f"- {color}")
+
