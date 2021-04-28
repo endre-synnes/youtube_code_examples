9b334864004fd0707d11dae437dd26fe350031ae
Endre Synnes <endre.m.synnes@gmail.com>
2021-04-28 09:34:00 +0200

    updated my first api gateway - POST

---
+++ lambda_intro/03_API_Gateway/05_first_api_gateway_POST.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    
+    bird = json.loads(event["body"])
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('awesome-birds')
+    
+    table.put_item(Item=bird)
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": f"{bird['name']} stored in DB"})
+    }
