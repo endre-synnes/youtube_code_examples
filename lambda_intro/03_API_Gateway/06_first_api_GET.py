4b94300a29f4fb225316046a011d8726844a450b
Endre Synnes <endre.m.synnes@gmail.com>
2021-05-05 19:48:19 +0200

    updated api gateway part two

---
+++ lambda_intro/03_API_Gateway/06_first_api_GET.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('awesome-birds')
+    
+    response = table.get_item(Key={'id': event["pathParameters"]["id"]})
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response['Item'])
+    }
