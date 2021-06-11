e6b5efcfa8b4c7f2dbea4fe7a3f54213264f2924
Endre Synnes <endre.m.synnes@gmail.com>
2021-06-11 10:37:53 +0200

    Updated serverless: api and lambda code

--- serverless/01_api_and_lambda/functions/get_bird.py
+++ serverless/01_api_and_lambda/functions/get_bird.py
@@ -13,13 +13,13 @@ BIRDS = [
 
 def lambda_handler(event, context):
   
-  bird_id = json.loads(event["pathParameters"]["id"])
+  bird_id = event["pathParameters"]["id"]
 
   for bird in BIRDS:
     if bird["id"] == bird_id:
       return {
           "statusCode": 200,
-          "body": json.dumps({bird})
+          "body": json.dumps(bird)
       }
   
 
