ce74e4e27b54dbbda3d1681ccdf07ba3a75b1c29
Endre Synnes <endre.m.synnes@gmail.com>
2021-11-16 13:26:02 +0100

    updated kinesis tutorial

--- lambda_intro/06_data_streams_notifications_and_queues/15_kinesis.py
+++ lambda_intro/06_data_streams_notifications_and_queues/15_kinesis.py
@@ -1,8 +1,20 @@
 import base64
+import json
+import boto3
+
 def lambda_handler(event, context):
-
   for record in event['Records']:
-
     #Kinesis data is base64 encoded so decode here
     payload=base64.b64decode(record["kinesis"]["data"])
-    print("Decoded payload: " + str(payload))
+    res = json.loads(payload)
+  
+    write_to_db(res)
+    print("Object successfully stored in DB.")
+       
+def write_to_db(data):
+  dynamodb = boto3.resource('dynamodb', region_name="eu-central-1")
+  table = dynamodb.Table("endres-test")
+
+  table.put_item(
+      Item=data
+  )
